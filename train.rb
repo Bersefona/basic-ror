6b93a65a732a48f6ccb9d0df859199a58d2a0626
Bersefona <bersefona@mail.ru>
2020-04-30 13:46:16 +0300

    Update train.rb

--- train.rb
+++ train.rb
@@ -1,31 +1,35 @@
 ###
+# TODO
+###
 #
-# (+) Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов,
-# (+) эти данные указываются при создании экземпляра класса
-# (+) Может набирать скорость
-# (+) Может возвращать текущую скорость
-# (+) Может тормозить (сбрасывать скорость до нуля)
-# (+) Может возвращать количество вагонов
-# (+) Может прицеплять/отцеплять вагоны (по одному вагону за операцию,
-# (+) метод просто увеличивает или уменьшает количество вагонов).
-# (+) Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
-# (+) Может принимать маршрут следования (объект класса Route). 
-# (+) При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
-# (+) Может перемещаться между станциями, указанными в маршруте.
-# (+) Перемещение возможно вперёд и назад, но только на 1 станцию за раз.
-# (+) Возвращать предыдущую станцию, текущую, следующую на основе маршрута.
+#   Разделить поезда на два типа PassengerTrain и CargoTrain (отдельными файлами)
+#   Сделать родителя для классов, который будет содержать общие методы и свойства
+#   (Train как родитель?)
+#   Разделить вагоны на класс "грузовые" и класс "пассажирские" (отдельными файлами)
+#   К пассажирскому поезду можно прицепить только пассажирские, к грузовому - грузовые.
+#   (Не забыть пояснить за protected!!!)
 #
 ###
 
 class Train
 
-  attr_reader :speed, :route, :number, :type, :current_station_index, :vans
+   TYPES = [
+    {
+      type: 'PassengerTrain',
+      name: 'пассажирский'
+    },
+    {
+      type: 'CargoTrain',
+      name: 'грузовой'
+    }
+  ]
+  
+  attr_reader :speed, :route, :number, :current_station_index
 
-  def initialize(number, type, vans)
+  def initialize(number)
     @number = number
-    @type = type
-    @vans = vans
     @speed = 0
+    @vans = []
     @route = nil
     @current_station_index = nil
   end
@@ -41,15 +45,15 @@ class Train
   def stop
     @speed = 0
   end
-
-  def add_van
-    @vans += 1 if @speed == 0
+  
+  def add_van(van)
+    @vans << van if @speed == 0
   end
-
+  
   def delete_van
-    @vans -= 1 if @vans > 0 && @speed == 0
+    @vans.pop if @speed == 0 && @vans.lenght > 0
   end
-
+  
   def route=(route)
     if route.is_a?(Route)
       @route = route
@@ -77,8 +81,9 @@ class Train
   def move_back
     move(self.current_station_index - 1) if prev_station
   end
-
-  private
+  
+  # чтобы методы нельзя было вызвать извне
+  protected
   
   def move(value)
     current_station.delete_train(self.number) 
