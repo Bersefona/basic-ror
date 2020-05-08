f7843c102816278fe28efe2f8369008c8e209e9a
Bersefona <bersefona@mail.ru>
2020-05-08 18:20:02 +0300

    Update passenger_train.rb

--- passenger_train.rb
+++ passenger_train.rb
@@ -2,7 +2,10 @@ require_relative 'train.rb'
 
 class PassengerTrain < Train
   
-
+  def initialize(name, type = 'passenger')
+    super
+  end
+ 
   def add_van(van)
     super(van) if van.is_a?(PassengerVan)
   end
