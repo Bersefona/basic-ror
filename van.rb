b7b66978d604d0016e48639fc40502c0cb5be499
Bersefona <bersefona@mail.ru>
2020-05-09 19:01:10 +0300

    Update van.rb

--- van.rb
+++ van.rb
@@ -1,10 +1,13 @@
 class Van
   
-  attr_accessor :number, :type
+  attr_reader :number
 
-  def initialize(number, type)
-    @number = number
-    @type = type
+  def initialize
+    @number = generate_number
+  end
+
+  def generate_number
+    srand.to_s.slice(0..9)
   end
   
 end
