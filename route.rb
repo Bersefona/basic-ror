8d1e2a2b8caeb6ce5a3b734d4b8ead921a51a5a3
Bersefona <bersefona@mail.ru>
2020-05-11 19:20:01 +0300

    Update route.rb

--- route.rb
+++ route.rb
@@ -1,6 +1,3 @@
-###
-# Подключить модуль InstanceCounter в класс маршрута.
-###
 require_relative 'instance_counter.rb'
 
 class Route
@@ -9,18 +6,38 @@ class Route
 
   def initialize(start, finish)
     @stations = [start, finish]
+    validate!
   end
 
   def add_station(station)
-    @stations.insert(-2, station) if station.is_a?(Station)
+    raise "В маршрут уже входит станция '#{station.name}'." if stations.include?(station)
+    raise StandardError, "Станция должна быть объектом типа Station." unless station.is_a?(Station)
+    @stations.insert(-2, station)
   end
   
   def delete_station
-    @stations.delete_at(-2) if self.stations.length > 2
+    raise "Отсутствуют промежуточные станции в маршруте." unless self.stations.length > 2
+    @stations.delete_at(-2) 
   end
   
   def show_stations
      @stations.each { |station| puts station }
+  end
+
+  protected
+
+  def validate!
+    raise "Начальная и конечная станции не должны совпадать." if self.stations[0] == self.stations[-1]
+    self.stations.each do |station|
+      raise StandardError "Станция должна быть объектом типа Station." unless station.is_a?(Station)
+    end
+  end
+
+  def valid?
+    validate!
+    true
+  rescue
+    false
   end
 
 end
