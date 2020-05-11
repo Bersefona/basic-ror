86a4952eaf336932ff7407d48f930424e99ecf2a
Bersefona <bersefona@mail.ru>
2020-05-11 19:22:08 +0300

    Update route.rb

--- route.rb
+++ route.rb
@@ -1,42 +1,38 @@
 require_relative 'instance_counter.rb'
 
-class Station 
+class Route
   include InstanceCounter
-  attr_reader :name, :trains
-  NAME_FORMAT = /^[a-zа-я]{1,50}([ \-][a-zа-я]{1,50})?([ \-][\d]{1,5})?$/i
+  attr_reader :stations
 
-  @@stations = []
-  
-  def self.all
-    @@stations
-  end
-
-  def initialize(name)
-    @name = name
-    @trains = []
+  def initialize(start, finish)
+    @stations = [start, finish]
     validate!
-    @@stations << self
   end
-  
-  def get_train(train)
-    raise StandardError "Можно разместить только поезд" unless train.is_a?(Train)
-    @trains[train.number.to_i] = train
-  end
-  
-  def delete_train(number)
-    raise StandardError "Не найден поезд с номером №#{number}" if self.trains[number].nil?
-    self.trains.delete(number)
-  end
-  
-  def types(type)
-    @trains.select {|train| train.type == type}
-  end  
 
-  protected 
+  def add_station(station)
+    raise "В маршрут уже входит станция '#{station.name}'." if stations.include?(station)
+    raise StandardError, "Станция должна быть объектом типа Station." unless station.is_a?(Station)
+
+    @stations.insert(-2, station)
+  end
   
+  def delete_station
+    raise "Отсутствуют промежуточные станции в маршруте." unless self.stations.length > 2
+
+    @stations.delete_at(-2) 
+  end
+  
+  def show_stations
+     @stations.each { |station| puts station }
+  end
+
+  protected
+
   def validate!
-    raise "Станция не может быть пустой." if self.name.nil?
-    raise StandardError, "Некорректное имя (#{self.name})" if self.name !~ NAME_FORMAT
+    raise "Начальная и конечная станции не должны совпадать." if self.stations[0] == self.stations[-1]
+    self.stations.each do |station|
+      raise StandardError "Станция должна быть объектом типа Station." unless station.is_a?(Station)
+    end
   end
 
   def valid?
@@ -45,5 +41,5 @@ class Station
   rescue
     false
   end
-  
-end 
+
+end
